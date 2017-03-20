c027a1a8f8cfa697329b83f3bc0c5f179bd0c6ea
Thang Ngoc <tnthang2910@gmail.com>
2017-03-20 13:04:55 +0700

    add picnic info to footer

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,7 +5,9 @@ class Message < ActiveRecord::Base
     raw_content = "TO ALL >>>"
     raw_content += "[info][title]New Confession (inlove)                                                   ※ Added at: #{created_at}[/title]"
     raw_content += content
-    raw_content += "[hr] Tâm sự với em tại đây nhé: https://goo.gl/IwkFKj"
+    raw_content += "[hr]- Tâm sự với em tại đây nhé: https://goo.gl/IwkFKj"
+    raw_content += "\n"
+    raw_content += "- Thông tin / đăng ký tham gia picnic tại đây nè các anh chị: https://www.chatwork.com/#!rid21036938-1863802595"
     raw_content += "[/info]"
     msg_content = URI.encode(raw_content)
     uri = "https://api.chatwork.com/v2/rooms/#{room_id}/messages?body=#{msg_content}"
